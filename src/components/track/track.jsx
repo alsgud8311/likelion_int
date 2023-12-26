c149cf6629f68c4ebb669d572319c168779f66c5
Minhyung Cho <dup33333@hufs.ac.kr>
2023-12-27 03:04:13 +0900

    design: marquee design

--- src/components/track/track.jsx
+++ src/components/track/track.jsx
@@ -29,15 +29,15 @@ const Track = () => {
                     <div style={{marginBottom: "10%"}}>프론트엔드 개발자는 사용자가 웹사이트나 어플리케이션에서 서비스를 이용하는 과정에서 만날 수 있는 모든 것들을 눈으로 볼 수 있게 구현하고, 사용자가 빠르고 편리하게 서비스를 이용할 수 있는 환경을 만들어 내는 일을 합니다.</div>
                     <div style={{fontSize: "25px", paddingBottom: '10px'}}>사용 기술스택</div>
                     <Styles.SkillsConatiner>
-                            <CSSIcon style={{height: '50px'}}/>
-                            <HtmlIcon style={{height: '50px'}}/>
-                            <JsIcon style={{height: '50px'}}/>
-                            <ReactIcon style={{height: '50px'}}/>
-                            <ReactRouterIcon style={{height: '50px'}}/>
-                            <GitIcon style={{height: '50px'}}/>
-                            <GithubIcon style={{height: '50px'}}/>
-                            <NpmIcon style={{height: '50px'}}/>
-                            <StyledComponentsIcon style={{height: '50px'}}/>
+                            <CSSIcon style={{height: '50px', marginRight: '15px'}}/>
+                            <HtmlIcon style={{height: '50px', marginRight: '15px'}}/>
+                            <JsIcon style={{height: '50px', marginRight: '15px'}}/>
+                            <ReactIcon style={{height: '50px', marginRight: '15px'}}/>
+                            <ReactRouterIcon style={{height: '50px', marginRight: '15px'}}/>
+                            <GitIcon style={{height: '50px', marginRight: '15px'}}/>
+                            <GithubIcon style={{height: '50px', marginRight: '15px'}}/>
+                            <NpmIcon style={{height: '50px', marginRight: '15px'}}/>
+                            <StyledComponentsIcon style={{height: '50px', marginRight: '15px'}}/>
                     </Styles.SkillsConatiner>
                 </Styles.DescriptionTextWrapper>
             </Styles.DescriptionWrapper>
