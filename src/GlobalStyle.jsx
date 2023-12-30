63294545a9d2b805c8935be24be70b197249eef5
Minhyung Cho <dup33333@hufs.ac.kr>
2023-12-30 15:34:20 +0900

    feat: font add

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -1,12 +1,21 @@
 import { createGlobalStyle } from "styled-components";
-import apple from './font/appleSDGothicNeoR.ttf'
+import apple from './font/appleSDGothicNeoR.ttf';
+import appleBold from './font/appleSDGothicNeoB.ttf';
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
           font-family: 'apple';
           src: local('apple'), local('apple');
+          font-weight: normal;
           font-style: normal;
           src: url(${apple}) format('truetype');
+      }
+
+    @font-face {
+        font-family: 'apple';
+        src: url(${appleBold}) format('truetype');
+        font-weight: bold; // 굵은 굵기
+        font-style: normal;
       }
       
   *, *::before, *::after {
